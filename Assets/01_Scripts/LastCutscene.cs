a24fd2b1ff64b580e951148d24c94082271b84a2
TheWolfHope <esperanzaycopito@gmail.com>
2024-10-31 14:36:21 -0300

    Avances
    - Cinematica done, fata testeo
    - Bugs de eventos, no dan gameloop

--- Assets/01_Scripts/LastCutscene.cs
+++ Assets/01_Scripts/LastCutscene.cs
@@ -8,15 +8,33 @@ public class LastCutscene : MonoBehaviour
     public PlayerMovement playerMovement;
     public CameraScript cameraScript;
     public ObjectivesManager objectivesManager;
-   
+
+    public Camera playerCamera;
+    public Transform playerArm;
+    public Animator camAnim;
+
+    private void Awake()
+    {
+        camAnim = FindObjectOfType<Animator>();
+        camAnim.enabled = false;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+
             playerMovement.canMove = false;
             cameraScript.canLook = false;
             objectivesManager.canSeeObj = false;
+
+            playerArm.gameObject.SetActive(false);
+            camAnim.enabled = true;
+
+            
             
         }
     }
+
+
 }
