973fab6b438528d6584eec03fb5f182de80508cc
TheWolfHope <esperanzaycopito@gmail.com>
2024-12-12 21:02:26 -0300

    Localization save

---
+++ Assets/01_Scripts/LocalizationChangeManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class LocalizationChangeManager : MonoBehaviour
+{
+    //private bool _active = false;
+
+    private void Start()
+    {
+        //int ID = Player
+    }
+}
